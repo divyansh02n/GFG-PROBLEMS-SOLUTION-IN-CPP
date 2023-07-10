0a882f90f12299f94637c870dce082101275b2b2
Divyansh Nakhate <138779643+divyansh02n@users.noreply.github.com>
2023-07-10 19:15:54 +0530

    Update Find-minimun-and-maxximum-element-in-an-array.cpp

--- Find-minimun-and-maxximum-element-in-an-array.cpp
+++ Find-minimun-and-maxximum-element-in-an-array.cpp
@@ -1,16 +1,17 @@
+
 pair<long long, long long> getMinMax(long long a[], int n) {
     int max=a[0];
     int min=a[0];
     
     for(int i=0; i<n; i++){
-        if(a[i]<max){
-            max=a[i];
+        if(a[i] < min ){
+            min = a[i];
         }
-    
-    if(a[i]>min){
-        min=a[i];
+        if(a[i] > max){
+            max = a[i];
+        }
     }
-    }
-    return{max,min};
-    
+    return{min,max};
+
+    // Time Complexity ---> O(n)
 }
