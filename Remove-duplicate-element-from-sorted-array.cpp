651021c995aacb488f7b9cf443287ba14689ab6f
Divyansh Nakhate <138779643+divyansh02n@users.noreply.github.com>
2023-07-11 15:15:16 +0530

    Add files via upload

---
+++ Remove-duplicate-element-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        int j = 1;
+        for(int i = 1; i < n; i++){
+            if(a[i] != a[i - 1]){
+                a[j] = a[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
